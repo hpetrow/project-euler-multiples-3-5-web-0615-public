ddd8bdd280e565bbd2a5b242b5c2445e74c04ee0
hpetrow <heather.petrow@gmail.com>
2015-07-01 11:57:34 -0400

    done

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,12 @@
 # Enter your procedural solution here!
+def collect_multiples(n)
+  (1..n-1).select{ |n|
+    n%3 == 0 || n%5 == 0
+  }
+end
+
+def sum_multiples(n)
+  collect_multiples(n).inject { |sum, n|
+    sum + n
+  }
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,20 @@
 # Enter your object-oriented solution here!
+class Multiples
+  attr_reader :max
+
+  def initialize(max)
+    @max = max
+  end
+
+  def collect_multiples
+    (1..self.max-1).select{ |n|
+      n%3 == 0 || n%5 == 0
+    }
+  end
+
+  def sum_multiples
+    self.collect_multiples.inject { |sum, n|
+      sum + n
+    }
+  end
+end
